f09c49814bf4b3acf439fc08616a5a6e70b98ac8
Volodymyr Hotsak <hotsak.volodymyr@gmail.com>
2023-08-16 20:00:42 -0400

    version 1.0

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const menuBtn = document.querySelector('.menu-btn'),
+  menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu--active');
+});
+
+/*modal*/
+
+const modalTriggers = document.querySelectorAll('[data-modal]'),
+  modalCloseBtn = document.querySelector('[data-close]'),
+  modal = document.querySelector('.modal');
+
+function showModal() {
+  modal.classList.add('show');
+  modal.classList.remove('hide');
+  document.body.style.overflow = 'hidden';
+}
+
+function hideModal() {
+  modal.classList.add('hide');
+  modal.classList.remove('show');
+  document.body.style.overflow = '';
+}
+
+modalTriggers.forEach((modal) => {
+  modal.addEventListener('click', showModal);
+});
+
+modalCloseBtn.addEventListener('click', hideModal);
+
+modal.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    hideModal();
+  }
+});
