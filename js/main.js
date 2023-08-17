de51aeace017efbd0157814c4a0390cb1887f51f
Volodymyr Hotsak <hotsak.volodymyr@gmail.com>
2023-08-17 19:19:08 -0400

    Nav Scroll

--- js/main.js
+++ js/main.js
@@ -34,3 +34,17 @@ modal.addEventListener('click', (e) => {
     hideModal();
   }
 });
+
+/*scroll*/
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const blockId = anchor.getAttribute('href');
+    document.querySelector('' + blockId).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  });
+}
